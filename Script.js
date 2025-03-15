8387298a7acf38a4e4fc05dea4a18dc57b33e4bc
id23mad <id23mad@cs.umu.se>
2025-03-15 11:33:17 +0100

    new commit

--- Script.js
+++ Script.js
@@ -162,3 +162,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+
+
--- randomizer.js
+++ randomizer.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
 	// En lista över bilder och beskrivningar till hemsidan.
 	const promotions = [
-		{img: "pic/p1.png", text: "Fånga de bästa blicken på ditt liv!"},
-		{img: "pic/p3.png", text: "Magiska ögonblick - Er bröllopsfotograf!"},
-		{img: "pic/p4.png", text: "Fånga kärleken och minnen!"}
+		{img: "pic/b1.png", text: "Fånga kärleken och minnen!"},
+		{img: "pic/w1.png", text: "Fånga de bästa blicken på ditt liv!"},
+		{img: "pic/w2.png", text: "Magiska ögonblick - Er bröllopsfotograf!"},
+		{img: "pic/w3.png", text: "Ert sagobröllop - Fångat i tidlösa bilder!"}
 	];
 
 	// Användning av const för att slumpa index baserat på antalet objekt i fältet.
@@ -13,4 +14,21 @@ document.addEventListener("DOMContentLoaded", function(){
 	document.getElementById("promo-text").innerText = promotions[randomIndex].text;
 	document.getElementById("promo-image").src = promotions[randomIndex].img;
 
-})
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const promotions = [
+        { img: "pic/b1.png", text: "Fånga kärleken och minnen!" },
+        { img: "pic/w1.png", text: "Fånga de bästa blicken på ditt liv!" },
+        { img: "pic/w2.png", text: "Magiska ögonblick - Er bröllopsfotograf!" },
+        { img: "pic/w3.png", text: "Fånga kärleken och minnen!" }
+    ];
+
+    const randomIndex = Math.floor(Math.random() * promotions.length);
+    const promoText = document.getElementById("promo-text");
+    const promoImage = document.getElementById("promo-image");
+
+    promoText.innerText = promotions[randomIndex].text;
+    promoImage.src = promotions[randomIndex].img;
+});
+
