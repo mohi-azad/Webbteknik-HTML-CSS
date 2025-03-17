159da32df4002b7ba82adc442c930abc717fddbc
id23mad <id23mad@cs.umu.se>
2025-03-17 11:43:30 +0100

    new commit

--- randomizer.js
+++ randomizer.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
 		{img: "pic/b1.png", text: "FÅNGA KÄRLEKEN OCH MINNEN!"},
 		{img: "pic/w1.png", text: "FÅNGA DE BÄSTA BLICKEN I LIVET!"},
 		{img: "pic/w5.png", text: "MAGISKA ÖGONBLICK - ER BRÖLLOPSFOTOGRAF!"},
-		{img: "pic/w6.png", text: "EET SAGABRÖLLOP - FÅNGATI TIDLÖSA BILDER!"}
+		{img: "pic/w6.png", text: "ETT SAGABRÖLLOP - FÅNGAT I TIDLÖSA BILDER!"}
 	];
 	// Användning av const för att slumpa index baserat på antalet objekt i fältet.
 	const randomIndex = Math.floor(Math.random() * promotions.length);
